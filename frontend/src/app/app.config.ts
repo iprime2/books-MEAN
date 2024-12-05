a0274939726132d0f5323572485af61f6cd7c915
Sushil Kumar Gupta <kalwarsushil2@gmail.com>
2024-12-05 23:59:06 +0530

    fix http depricated

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,14 +2,14 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { provideRouter } from '@angular/router';
 import { provideToastr, ToastrModule } from 'ngx-toastr';
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(),
     importProvidersFrom(ToastrModule.forRoot()),
     importProvidersFrom(BrowserAnimationsModule),
     provideToastr()
